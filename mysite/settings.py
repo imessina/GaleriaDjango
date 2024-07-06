90220617696240f4dcec31d6741608a16f33a86e
Nacho Messina <137566335+imessina@users.noreply.github.com>
2024-07-06 12:06:19 -0400

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria'
+    'galeria' #aqui agregamos nuestro proyecto galeria
 ]
 
 MIDDLEWARE = [
